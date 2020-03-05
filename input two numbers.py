40f76bddb606f65f9e4a932a59098805106e1744
Lemachuca10 <60229283+Lemachuca10@users.noreply.github.com>
2020-03-05 12:01:03 -0600

    Add files via upload

---
+++ input two numbers.py
@@ -0,0 +1,20 @@
+#Luis Machuca
+#3/5/2020
+#This program will take two inputs from a user and checks whter they are equal or not.
+def numberchoice():
+    num1 = input("Enter 1 number?")
+    num2 = input("Enter a second number?")
+    if num2==num1:
+        print(num2,'is equal to', num1,'which makes it True')
+        print("Good job!")
+    else:
+        print(num2,'is not equal to', num1,'which makes it False')
+        print("Sorry, buddy that is not right")
+numberchoice()
+
+
+
+   
+    
+    
+    
